e36e700779baee519eddd885c6fdc5a03babbbb2
Akhmad Rizki Prayoga <akhmadrizki24@gmail.com>
2022-10-24 17:59:09 +0800

    Add filter pricing

--- app/Http/Controllers/Dashboard/DashboardController.php
+++ app/Http/Controllers/Dashboard/DashboardController.php
@@ -3,12 +3,47 @@
 namespace App\Http\Controllers\Dashboard;
 
 use App\Http\Controllers\Controller;
+use App\Models\Pembayaran;
+use App\Models\Pengeluaran;
+use App\Models\Penyewa;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class DashboardController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
-        return view('pages.index');
+        $pedagang = Penyewa::all();
+        $admin = User::where('role_id', '=', 2)->get();
+
+        // Request
+        $dateFilter = $request->date;
+        $monthFilter = $request->month;
+        $yearFilter = $request->year;
+
+        if ($request->has('date') || $request->has('month') || $request->has('year')) {
+            $pemasukan = Pembayaran::where('tanggal', $dateFilter)
+                ->whereMonth('tanggal', $monthFilter)
+                ->whereYear('tanggal', $yearFilter)
+                ->get();
+
+            $pengeluaran = Pengeluaran::where('tanggal', $dateFilter)
+                ->whereMonth('tanggal', $monthFilter)
+                ->whereYear('tanggal', $yearFilter)
+                ->get();
+        } else {
+            $pemasukan = Pembayaran::all();
+            $pengeluaran = Pengeluaran::all();
+        }
+
+        return view('pages.index', compact(
+            'pemasukan',
+            'pengeluaran',
+            'pedagang',
+            'admin',
+            'dateFilter',
+            'monthFilter',
+            'yearFilter',
+        ));
     }
 }
--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -19,130 +19,144 @@ Dashboard
 <div class="page-content">
   <section class="row">
     <div class="col-12">
-      <div class="card">
-        <div class="card-body">
-          <div class="row">
-            <div class="col-4">
-              <div class="form-group">
-                <label for="email-id-icon">Harian</label>
-                <input type="date" class="form-control">
+      <form action="" method="GET">
+        <div class="card">
+          <div class="card-body">
+            <div class="row">
+              <div class="col-4">
+                <div class="form-group">
+                  <label for="email-id-icon">Harian</label>
+                  <input name="date" type="date" class="form-control">
+                </div>
               </div>
-            </div>
 
-            <div class="col-4">
-              <div class="form-group">
-                <label for="email-id-icon">Bulanan</label>
-                <select class="form-select">
-                  <option value="none" selected disabled>- Pilih Bulan -</option>
-                  <option value="01">Januari</option>
-                  <option value="02">Februari</option>
-                  <option value="03">Maret</option>
-                  <option value="04">April</option>
-                  <option value="05">Mei</option>
-                  <option value="06">Juni</option>
-                  <option value="07">Juli</option>
-                  <option value="08">Agustus</option>
-                  <option value="09">September</option>
-                  <option value="10">Oktober</option>
-                  <option value="11">November</option>
-                  <option value="12">Desember</option>
-                </select>
+              <div class="col-4">
+                <div class="form-group">
+                  <label for="email-id-icon">Bulanan</label>
+                  <select name="month" class="form-select">
+                    <option value="none" selected disabled>- Pilih Bulan -</option>
+                    <option value="01">Januari</option>
+                    <option value="02">Februari</option>
+                    <option value="03">Maret</option>
+                    <option value="04">April</option>
+                    <option value="05">Mei</option>
+                    <option value="06">Juni</option>
+                    <option value="07">Juli</option>
+                    <option value="08">Agustus</option>
+                    <option value="09">September</option>
+                    <option value="10">Oktober</option>
+                    <option value="11">November</option>
+                    <option value="12">Desember</option>
+                  </select>
+                </div>
               </div>
-            </div>
 
-            <div class="col-4">
-              <div class="form-group">
-                <label for="email-id-icon">Tahunan</label>
-                <select class="form-select">
-                  <option value="none" selected disabled>- Pilih Tahun -</option>
-                  <option>IT</option>
-                  <option>Blade Runner</option>
-                  <option>Thor Ragnarok</option>
-                </select>
+              <div class="col-4">
+                <div class="form-group">
+                  <label for="email-id-icon">Tahunan</label>
+                  <select name="year" id="year" class="form-control">
+                    @php
+                    $year = date('Y');
+                    @endphp
+
+                    @for ($i = $year; $i <= $year + 8; $i++) <option value="{{ $i }}">{{ $i }}</option>
+                      @endfor
+
+                  </select>
+                </div>
               </div>
-            </div>
 
-            <div class="col-12 d-flex justify-content-center">
-              <button type="button" class="btn btn-primary me-1 mb-1">Terapkan</button>
-            </div>
+              <div class="col-12 d-flex justify-content-center">
+                <button type="submit" class="btn btn-primary me-1 mb-1">Terapkan</button>
+              </div>
 
+            </div>
           </div>
         </div>
-      </div>
+      </form>
     </div>
 
+    {{-- stats session --}}
     <div class="col-12">
       <div class="row">
         <div class="col-6 col-lg-3 col-md-6">
           <div class="card">
             <div class="card-body px-3 py-4-5">
               <div class="row">
-                <div class="col-md-4">
-                  <div class="stats-icon purple">
-                    <i class="iconly-boldShow"></i>
-                  </div>
-                </div>
-                <div class="col-md-8">
-                  <h6 class="text-muted font-semibold">Profile Views</h6>
-                  <h6 class="font-extrabold mb-0">112.000</h6>
+                <div class="col-md-12">
+                  <h6 class="text-muted font-semibold">
+                    Pemasukan
+                    @if (isset($dateFilter))
+                    {{ date( 'd/m/Y', strtotime($dateFilter)) }}
+                    @elseif(isset($monthFilter))
+                    {{ date( 'F', strtotime($monthFilter)) }}
+                    @endif
+
+                  </h6>
+                  @php
+                  $uangMasuk = $pemasukan->sum('biaya_sewa');
+                  @endphp
+                  <h6 class="font-extrabold mb-0">Rp{{ number_format($uangMasuk, 0, ',', '.') }}</h6>
+
+                  <a href="#">Lihat Detail &rarr;</a>
                 </div>
               </div>
             </div>
           </div>
         </div>
+
         <div class="col-6 col-lg-3 col-md-6">
           <div class="card">
             <div class="card-body px-3 py-4-5">
               <div class="row">
-                <div class="col-md-4">
-                  <div class="stats-icon blue">
-                    <i class="iconly-boldProfile"></i>
-                  </div>
-                </div>
-                <div class="col-md-8">
-                  <h6 class="text-muted font-semibold">Followers</h6>
-                  <h6 class="font-extrabold mb-0">183.000</h6>
+                <div class="col-md-12">
+                  <h6 class="text-muted font-semibold">Pengeluaran {{ date('F') }} {{ date('Y') }}</h6>
+                  @php
+                  $uangKeluar = $pengeluaran->sum('total');
+                  @endphp
+                  <h6 class="font-extrabold mb-0">Rp{{ number_format($uangKeluar, 0, ',', '.') }}</h6>
+
+                  <a href="#">Lihat Detail &rarr;</a>
                 </div>
               </div>
             </div>
           </div>
         </div>
+
         <div class="col-6 col-lg-3 col-md-6">
           <div class="card">
             <div class="card-body px-3 py-4-5">
               <div class="row">
-                <div class="col-md-4">
-                  <div class="stats-icon green">
-                    <i class="iconly-boldAdd-User"></i>
-                  </div>
-                </div>
-                <div class="col-md-8">
-                  <h6 class="text-muted font-semibold">Following</h6>
-                  <h6 class="font-extrabold mb-0">80.000</h6>
+                <div class="col-md-12">
+                  <h6 class="text-muted font-semibold">Jumlah Pedagang</h6>
+                  <h6 class="font-extrabold mb-0">{{ count($pedagang) }}</h6>
+
+                  <a href="#">Lihat Detail &rarr;</a>
                 </div>
               </div>
             </div>
           </div>
         </div>
+
         <div class="col-6 col-lg-3 col-md-6">
           <div class="card">
             <div class="card-body px-3 py-4-5">
               <div class="row">
-                <div class="col-md-4">
-                  <div class="stats-icon red">
-                    <i class="iconly-boldBookmark"></i>
-                  </div>
-                </div>
-                <div class="col-md-8">
-                  <h6 class="text-muted font-semibold">Saved Post</h6>
-                  <h6 class="font-extrabold mb-0">112</h6>
+                <div class="col-md-12">
+                  <h6 class="text-muted font-semibold">Jumlah Admin</h6>
+                  <h6 class="font-extrabold mb-0">{{ count($admin) }}</h6>
+
+                  <a href="#">Lihat Detail &rarr;</a>
                 </div>
               </div>
             </div>
           </div>
         </div>
+
       </div>
     </div>
+    {{-- end stats session --}}
+
   </section>
 </div>
 @endsection
