72e82ab737975787147a6cf7713ae1e6b603d133
akhmadrizki <akhmadrizki24@gmail.com>
2023-03-25 09:29:09 +0800

    Change color in export excel column

--- resources/views/exports/laporan/bulanan.blade.php
+++ resources/views/exports/laporan/bulanan.blade.php
@@ -14,8 +14,8 @@
             <th style="border: 1px solid black; font-weight:bold">Keterangan</th>
             <th style="border: 1px solid black; font-weight:bold">Operator</th>
             <th style="border: 1px solid black; font-weight:bold; background-color: #baf7b1;">Pemasukan</th>
-            <th style="border: 1px solid black; font-weight:bold; background-color: #dc3546">Pengeluaran</th>
-            <th style="border: 1px solid black; font-weight:bold; background-color: #435ebe">Saldo</th>
+            <th style="border: 1px solid black; font-weight:bold; background-color: #fa949e">Pengeluaran</th>
+            <th style="border: 1px solid black; font-weight:bold; background-color: #90a8ff">Saldo</th>
         </tr>
     </thead>
     <tbody>
@@ -28,10 +28,10 @@
             <td style="border: 1px solid black; background-color: #baf7b1">
                 {{$keuangan->pemasukan }}
             </td>
-            <td style="border: 1px solid black; background-color: #dc3546">
+            <td style="border: 1px solid black; background-color: #fa949e">
                 {{ $keuangan->pengeluaran }}
             </td>
-            <td style="border: 1px solid black; background-color: #435ebe">
+            <td style="border: 1px solid black; background-color: #90a8ff">
                 {{ $keuangan->saldo }}
             </td>
         </tr>
@@ -41,10 +41,10 @@
             <th class="text-success" style="border: 1px solid black; background-color: #baf7b1">
                 {{ $uangmasuk }}
             </th>
-            <th class="text-danger" style="border: 1px solid black; background-color: #dc3546">
+            <th class="text-danger" style="border: 1px solid black; background-color: #fa949e">
                 {{ $uangkeluar }}
             </th>
-            <th class="text-dark" style="border: 1px solid black; background-color: #435ebe">
+            <th class="text-dark" style="border: 1px solid black; background-color: #90a8ff">
                 @if (!$totalsaldo)
                 0
                 @else
